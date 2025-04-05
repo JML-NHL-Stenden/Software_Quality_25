7776094c22028d025fd9209e596d32bc12a85998
JML-NHL-Stenden <jia.men.lam@student.nhlstenden.com>
2025-04-05 15:19:41 +0200

    fixed issue

--- Jabberpoint-IT/src/model/BitmapItem.java
+++ Jabberpoint-IT/src/model/BitmapItem.java
@@ -16,8 +16,7 @@ import java.awt.Color;
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
-public class BitmapItem extends SlideItem
-{
+public class BitmapItem extends SlideItem {
 
     private BufferedImage bufferedImage;
     private final String imageName;
@@ -26,35 +25,29 @@ public class BitmapItem extends SlideItem
     protected static final String NOT_FOUND = " not found";
 
     // level is equal to item-level; name is the name of the file with the Image
-    public BitmapItem(int level, String name)
-    {
+    public BitmapItem(int level, String name) {
         super(level);
         imageName = name;
-        try
-        {
+        try {
             bufferedImage = ImageIO.read(new File(imageName));
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             System.err.println(FILE + imageName + NOT_FOUND);
         }
     }
 
     // An empty bitmap-item
-    public BitmapItem()
-    {
+    public BitmapItem() {
         this(0, null);
     }
 
     // Returns the filename of the image
-    public String getName()
-    {
+    public String getName() {
         return imageName;
     }
 
     // Computes the bounding box of the image
     public Rectangle getBoundingBox(Graphics g, ImageObserver observer,
-            float scale, Style style)
-    {
+            float scale, Style style) {
         int width = (int) (bufferedImage.getWidth(observer) * scale);
         int height = (int) (bufferedImage.getHeight(observer) * scale);
         int x = (int) (style.indent * scale);
@@ -64,10 +57,8 @@ public class BitmapItem extends SlideItem
 
     // Draws the image on the slide
     public void draw(int x, int y, float scale, Graphics g,
-            Style style, ImageObserver observer)
-    {
-        if (bufferedImage == null)
-        {
+            Style style, ImageObserver observer) {
+        if (bufferedImage == null) {
             g.setColor(Color.RED);
             g.drawString("Image not found: " + imageName, x, y);
             return;
@@ -79,8 +70,7 @@ public class BitmapItem extends SlideItem
         g.drawImage(bufferedImage, drawX, drawY, width, height, observer);
     }
 
-    public String toString()
-    {
+    public String toString() {
         return "BitmapItem[" + getLevel() + "," + imageName + "]";
     }
 }
